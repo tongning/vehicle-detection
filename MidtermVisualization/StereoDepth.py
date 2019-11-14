dd8cf02d95c587fe9780306232f212cbd3dd24e8
Eric Chan <ericchan@DN0a22b0d9.SUNet>
2019-11-14 15:43:00 -0800

    fixed bug in StereoDepth

--- MidtermVisualization/StereoDepth.py
+++ MidtermVisualization/StereoDepth.py
@@ -15,7 +15,6 @@ class Convert3D:
             self.img_r = img_r
 
         self.bounds_list = bounds_list
-
         self.depth_img = self._CalculateStereoDisparityFast()
         self.xyz_img = self._XYZImageFromDisparity()
         self.point_cloud = self.PointCloud()
@@ -53,14 +52,7 @@ class Convert3D:
 
         displ = left_matcher.compute(self.img_l, self.img_r).astype(np.float32)/16.0
         dispr = right_matcher.compute(self.img_r, self.img_l).astype(np.float32)/16.0
-        #displ = np.int16(displ)
-        #dispr = np.int16(dispr)
         filteredImg = wls_filter.filter(displ, self.img_l, None, dispr)
-
-        #filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0,
-        #                            alpha=255, norm_type=cv2.NORM_MINMAX
-        #                           );
-        #filteredImg = np.uint8(filteredImg)
 
         return filteredImg
 
@@ -91,19 +83,7 @@ class Convert3D:
         cx1 = 526.242457
         cy = 1.728540000000e+02
         Tx = -120.00
-        """
-        Q = np.array([ # Obtain using stereo rectify?
-            [1.0, 0, 0, -cx],
-            [0, 1.0, 0, -cy],
-            [0, 0, 0, f],
-            [0, 0, -1.0/Tx, (cx - cx1) / cx]])
 
-
-        Q2 = np.float32([[1, 0, 0, 0],
-                    [0, -1, 0, 0],
-                    [0, 0, f*0.01, 0],
-                    [0, 0, 0, 1]])
-        """
         Q = np.float32([[   1.,            0.,            0.,         -614.37893072],
             [   0.,            1.,            0.,         -162.12583194],
             [   0.,            0.,            0.,          680.05186262],
@@ -114,22 +94,22 @@ class Convert3D:
 
     def PointCloud(self):
         mask = self.depth_img > self.depth_img.min()
-        #out_points = np.int16(self.xyz_img[mask] * 10)
         return self.xyz_img[mask]
-        #pcd = o3d.geometry.PointCloud()
-        #pcd.points = o3d.utility.Vector3dVector(out_points)
-        #o3d.visualization.draw_geometries([pcd])
 
     def Positions3D(self):
         positions = []
 
         for bound in self.bounds_list:
-            if (bound[4] == 'car' and float(bound[5]) > 0.9):
+            if (bound[4] == 'car' and float(bound[5]) > 0.4):
                 xyz_car_img = self.xyz_img[int(bound[0]):int(bound[1]), int(bound[2]):int(bound[3])]
                 h, w, _ = xyz_car_img.shape
                 xyz_car_img = xyz_car_img[int(h/4):int(3*h/4), int(w/4):int(3*w/4)]
 
                 # Get point
+                cp = np.median(xyz_car_img.reshape(-1, 3), axis=0)
+                print("-----------------")
+                print("median filtered: ", cp)
+                print("raw            : ", car_pos)
                 car_pos = (xyz_car_img[int(xyz_car_img.shape[0]/2), int(xyz_car_img.shape[1]/2)]).tolist()
                 car_pos[2] -= 2
                 positions.append(car_pos)
