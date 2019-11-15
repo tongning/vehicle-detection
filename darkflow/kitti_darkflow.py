c0acee6349bbca8c5b3d216594a567eaa7bb6656
Eric Chan <ericchan@DN0a1e4fd8.SUNet>
2019-11-14 19:26:23 -0800

    added network training files

---
+++ darkflow/kitti_darkflow.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append("..")
+
+from darkflow.net.build import TFNet
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+
+
+def boxing(original_img, predictions):
+    newImage = np.copy(original_img)
+    print(predictions)
+    for result in predictions:
+        top_x = result['topleft']['x']
+        top_y = result['topleft']['y']
+
+        btm_x = result['bottomright']['x']
+        btm_y = result['bottomright']['y']
+
+        confidence = result['confidence']
+        label = result['label'] + " " + str(round(confidence, 3))
+
+        if confidence > 0:
+            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (255,0,0), 3)
+            newImage = cv2.putText(newImage, label, (top_x, top_y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL , 0.8, (0, 230, 0), 1, cv2.LINE_AA)
+
+    return newImage
+
+
+options = {"model": "cfg/yolo.cfg",
+           "load": "weights/yolo.weights",
+           "threshold": 0.25,
+           "gpu": 0.8}
+
+os.chdir("darkflow")
+tfnet = TFNet(options)
+
+
+directory_l = "../data_tracking_image_2/testing/image_02/0000"
+
+for i, filename in enumerate(sorted(os.listdir(directory_l))):
+    if filename.endswith('.png'):
+        filename_l = os.path.join(directory_l, filename)
+        frame = cv2.imread(filename_l)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = tfnet.return_predict(frame)
+        _, ax = plt.subplots(figsize=(20, 10))
+        ax.imshow(boxing(frame, results))
+        plt.show()
+        #plt.pause(0.1)
+        #plt.draw()
+
+#%matplotlib inline
+plt.show()
